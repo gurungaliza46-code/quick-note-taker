45c18009bb99b06cf96bc9fa9425ec891c326a20
gurungaliza46-code <gurungaliza46@gmail.com>
2026-05-14 11:35:31 +0900

    Enhance electronAPI with note management features
    Added new methods for managing notes and enhanced menu actions.

--- preload.js
+++ preload.js
@@ -3,9 +3,28 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     saveNote: (text) => ipcRenderer.invoke('save-note', text),
     loadNote: () => ipcRenderer.invoke('load-note'),
+
     saveAs: (text) => ipcRenderer.invoke('save-as', text),
-    newNote: () => ipcRenderer.invoke('new-note'),
+    newNote: (hasUnsavedChanges) => ipcRenderer.invoke('new-note', hasUnsavedChanges),
     openFile: () => ipcRenderer.invoke('open-file'),
-    smartSave: (text, filepath) => ipcRenderer.invoke('smart-save', text, filepath),
-    onMenuAction: (channel, callback) => ipcRenderer.on(channel, callback)
+    smartSave: (text, filePath) => ipcRenderer.invoke('smart-save', text, filePath),
+
+    getNotes: () => ipcRenderer.invoke('get-notes'),
+    saveJsonNote: (note) => ipcRenderer.invoke('save-json-note', note),
+    deleteJsonNote: (noteId) => ipcRenderer.invoke('delete-json-note', noteId),
+
+    deleteAllNotes: () => ipcRenderer.invoke('delete-all-notes'),
+
+    onMenuAction: (channel, callback) => {
+        const validChannels = [
+            'menu-new-note',
+            'menu-open-file',
+            'menu-save',
+            'menu-save-as'
+        ];
+
+        if (validChannels.includes(channel)) {
+            ipcRenderer.on(channel, callback);
+        }
+    }
 });
